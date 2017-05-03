54091a7f34c4f97e6c7e0a99ec134a1075531c7e
kingardhya <ardhyaputra@gmail.com>
2017-05-03 06:42:54 +0000

    [message]

---
+++ formgias/import_fg/doctype/laporan_container_per_shipper/laporan_container_per_shipper.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, Frappe and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Laporan Container per Shipper', {
+	refresh: function(frm) {
+
+	}
+});
