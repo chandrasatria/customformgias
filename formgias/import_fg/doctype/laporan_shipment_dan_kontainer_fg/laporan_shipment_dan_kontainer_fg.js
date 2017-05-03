19accfebc1365f015993f021fa6d8cb58dfe486a
kingardhya <ardhyaputra@gmail.com>
2017-05-03 04:49:29 +0000

    [message]

---
+++ formgias/import_fg/doctype/laporan_shipment_dan_kontainer_fg/laporan_shipment_dan_kontainer_fg.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, Frappe and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Laporan Shipment dan Kontainer fg', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ formgias/import_fg/doctype/laporan_untuk_import_shipment_fg/laporan_untuk_import_shipment_fg.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, Frappe and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Laporan untuk Import Shipment fg', {
+	refresh: function(frm) {
+
+	}
+});
