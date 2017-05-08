5856ff97ace31f75ef020bb4e89054fedb195cce
root <root@leo-coba-coba.localdomain>
2017-05-08 02:36:49 +0000

    [message]

---
+++ formgias/hrd_fg/doctype/pengajuan_uang_jasa_dan_sisa_gaji_karyawan/pengajuan_uang_jasa_dan_sisa_gaji_karyawan.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2016, Frappe and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Pengajuan Uang Jasa dan Sisa Gaji Karyawan', {
+	refresh: function(frm) {
+		//cur_frm.cscript.custom_validate = function(doc) {
+		frappe.ui.form.on("Pengajuan Uang Jasa dan Sisa Gaji Karyawan","sisa_kewajiban",function(frm, doctype, name){
+			//if (cur_frm.doc.nominal != '' && cur_frm.doc.hutang_karyawan != '' && cur_frm.doc.sisa_kewajiban !='' ){
+				var row = locals[doctype][name];
+				row.net=row.nominal-(row.hutang_karyawan+row.sisa_kewajiban);
+				refresh_field("net");
+			//}
+		});
+
+		frappe.ui.form.on("Pengajuan Uang Jasa dan Sisa Gaji Karyawan","quit_date",function(){
+			cur_frm.doc.lama_kerja = frappe.datetime.get_day_diff(cur_frm.doc.quit_date,cur_frm.doc.entry_date);
+			refresh_field("lama_kerja");
+		});
+
+
+	}
+});
