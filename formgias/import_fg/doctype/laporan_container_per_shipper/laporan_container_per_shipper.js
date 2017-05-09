7ca66ff31ce58260d9920fb7ce4dd9fe482a949a
root <root@leo-coba-coba.localdomain>
2017-05-09 03:13:04 +0000

    [message]

--- formgias/import_fg/doctype/laporan_container_per_shipper/laporan_container_per_shipper.js
+++ formgias/import_fg/doctype/laporan_container_per_shipper/laporan_container_per_shipper.js
@@ -3,6 +3,13 @@
 
 frappe.ui.form.on('Laporan Container per Shipper', {
 	refresh: function(frm) {
+		frappe.ui.form.on("Kota dan Bulan fg","desember",function(frm, cdt, cdn){
+			//if (cur_frm.doc.nominal != '' && cur_frm.doc.hutang_karyawan != '' && cur_frm.doc.sisa_kewajiban !='' ){
+				var row = locals[cdt][cdn];
+				row.total=row.januari+row.februari+row.maret+row.april+row.mei+row.juni+row.juli+row.agustus+row.september+row.oktober+row.november+row.desember;
+				refresh_field("total");
+			//}
+		});
 
 	}
 });
